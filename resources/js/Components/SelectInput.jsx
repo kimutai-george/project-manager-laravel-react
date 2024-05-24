5080c2b4ed079187c5c773ff8c069930e3dc367d
George Kimutai <georgekimuu@gmail.com>
2024-05-24 18:59:45 +0300

    Project Layout

---
+++ resources/js/Components/SelectInput.jsx
@@ -0,0 +1,23 @@
+import { forwardRef, useRef } from 'react';
+
+export default forwardRef(function SelectInput({ className = '', children, ...props }, ref) {
+    const input = ref ? ref : useRef();
+
+
+
+    return (
+        <select
+            {...props}
+            className={
+                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
+                className
+            }
+            ref={input}
+        >
+            <option value="">Select Status</option>
+            <option value="pending">Pending</option>
+            <option value="in_progress">In Progress</option>
+            <option value="complete">Complete</option>
+        </select>
+    );
+});
