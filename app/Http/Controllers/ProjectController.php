a0e1f42f329336869e0ad34d1b86cbbbb90b1c7c
George Kimutai <georgekimuu@gmail.com>
2024-05-26 19:09:48 +0300

    Delete Project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -107,6 +107,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $name = $project->name;
+        $project->delete();
+        return to_route('project.index')->with('success',"Project $name Was Deleted");
     }
 }
