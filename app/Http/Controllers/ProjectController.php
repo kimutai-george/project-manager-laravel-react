658f7bc6414f3231780328dcddf1447ea7b51aeb
George Kimutai <georgekimuu@gmail.com>
2024-06-27 19:53:18 +0300

    Thursday chnages

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProjectController extends Controller
@@ -91,7 +92,9 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return inertia('Project/Edit',[
+            'project' => new ProjectResource($project)
+        ]);
     }
 
     /**
@@ -99,7 +102,19 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $data = $request->validated();
+        $image = $data['image'] ?? null; // Image:: Illuminate\Http\UploadedFile
+        $data['updated_by'] = Auth::id();
+        if($image)
+        {
+            if($project->image_path)
+            {
+                Storage::disk('public')->delete($project->image_path);
+            }
+           $data['image_path'] = $image->store('project/'.Str::random(),'public');
+        }
+        $project->update();
+        return to_route('project.index')->with('success',"Project $project->name Was Updated");
     }
 
     /**
